788f18273280d754eac91f7f7a4190a414cf27c1
kaushikvenna <vennakaushikramkoteshwar@gmail.com>
2023-11-28 17:14:45 -0600

    modified icecream models

--- models/icecream.js
+++ models/icecream.js
@@ -2,6 +2,11 @@ const mongoose = require("mongoose")
 const icecreamSchema = mongoose.Schema({
 flavour: String,
 quantity: Number,
-price: Number
+price: {
+    type: Number,
+    min: [0, 'icecream price must be greater than 0'],  
+    max: [15, 'icecream price must be less than 15'], 
+    required: [true, 'icecream price is required'] 
+}
 })
 module.exports = mongoose.model("icecream",icecreamSchema)
